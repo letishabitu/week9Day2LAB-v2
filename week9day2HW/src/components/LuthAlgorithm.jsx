6fb18cb7ae26680935a04187fb68794f241c0331
letishabitu <lkidanu@gmail.com>
2023-11-19 16:50:04 -0500

    secondcommit

---
+++ week9day2HW/src/components/LuthAlgorithm.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Form from './Form'
+
+function LuthAlgorithm(props) {
+
+
+  // const luhnCheck = num => {
+  //   let arr = (num + '')
+  //     .split('')
+  //     .reverse()
+  //     .map(x => parseInt(x));
+  //   let lastDigit = arr.splice(0, 1)[0];
+  //   let sum = arr.reduce((acc, val, i) => (i % 2 !== 0 ? acc + val : acc + ((val * 2) % 9) || 9), 0);
+  //   sum += lastDigit;
+  //   return sum % 10 === 0;
+  // };
+// const inputCard = luhnCheck(num);
+// console.log(luhnCheck());
+// if (props.validityCheck === "true")
+   
+    
+    
+    
+
+  return (
+    <div>
+     <Form />
+     
+    
+     
+     
+    </div>
+  )
+}
+
+export default LuthAlgorithm
